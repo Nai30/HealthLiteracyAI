439e3c6d0bbf0dfec8c0b282d82b8c7cf1b2c336
Nai30 <152220400+Nai30@users.noreply.github.com>
2026-03-14 12:19:35 -0400

    Initial Commit

---
+++ HealthLiteracyAI/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  HealthLiteracyAI
+//
+//  Created by Naima Marseille on 3/14/26.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController {
+    let camera = Camera()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // REMOVE 'func' and '()' from Task
+        Task { @MainActor in
+            let isAuthorized = await camera.checkCameraAuthorization()
+            
+            if isAuthorized {
+                let success = camera.setup()
+                
+                if success {
+                    // Use the session from your Camera.swift file
+                    let previewLayer = AVCaptureVideoPreviewLayer(session: camera.session)
+                    previewLayer.frame = self.view.bounds
+                    previewLayer.videoGravity = .resizeAspectFill
+                    self.view.layer.addSublayer(previewLayer)
+                    
+                    print("Camera is now running!")
+                }
+            }
+        }
+    }
+}
+
+
+
+
