e7db143c26f3c4ca9d6ea66964448750981fab0f
Nai30 <152220400+Nai30@users.noreply.github.com>
2026-03-14 12:19:35 -0400

    Initial Commit

---
+++ HealthLiteracyAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthLiteracyAI
+//
+//  Created by Naima Marseille on 3/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
